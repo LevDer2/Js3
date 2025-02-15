12b2997aa256cfca3321f018e8b4c46ee737cda7
LevDer2 <123574272+LevDer2@users.noreply.github.com>
2025-02-15 09:39:13 +0200

    Js

---
+++ js/string.js
@@ -0,0 +1,21 @@
+// Task 1
+
+const result = 5 + 5 + '5';
+console.log(result);
+console.log(typeof(result));
+
+// Task 2
+
+// Task 3
+
+const my = "My";
+const name = "name";
+const is = "is";
+const fullName = my + " " + name + " " + is + " Viktor";
+console.log(fullName);
+
+// Task 4
+
+const userName = prompt("Введіть своє ім'я");
+const payment = 300;
+const rest = alert(`${userName}! До сплати ${payment} ,гривень`)
